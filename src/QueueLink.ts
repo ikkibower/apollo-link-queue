5b9635dc56eb46e5ea5c5acefd3143b1a00d3141
Elliott Chalmers <chalmersej@gmail.com>
2019-12-03 07:38:15 -0500

    update: add wtrocki:persitence-filtering from parent pr #14

--- src/QueueLink.ts
+++ src/QueueLink.ts
@@ -1,54 +1,140 @@
 import {
-    ApolloLink,
-    Observable,
-    Operation,
-    FetchResult,
-    NextLink,
-} from 'apollo-link';
-import { Observer } from 'zen-observable-ts';
+  ApolloLink,
+  Observable,
+  Operation,
+  FetchResult,
+  NextLink
+} from "apollo-link";
+import { Observer } from "zen-observable-ts";
+
+export interface PersistentStore<T> {
+  getItem: (key: string) => Promise<T> | T;
+  setItem: (key: string, data: T) => Promise<void> | void;
+  removeItem: (key: string) => Promise<void> | void;
+}
+
+export type PersistedData = string | null;
 
 interface OperationQueueEntry {
-    operation: Operation;
-    forward: NextLink;
-    observer: Observer<FetchResult>;
-    subscription?: { unsubscribe: () => void };
+  operation: Operation;
+  forward: NextLink;
+  observer: Observer<FetchResult>;
+  subscription?: { unsubscribe: () => void };
+}
+
+/**
+ * Initialization options for QueueLink
+ */
+interface QueueLinkOptions {
+  /**
+   * store used to persist queue.
+   */
+  store?: {
+    // window.localstore, AsyncStore or anything that conforms to interface
+    engine: PersistentStore<PersistedData>;
+    // key used to store data
+    storeKey: string | "apollo-link-queue";
+  };
+  /**
+   * Queue only specific operations
+   **/
+  filter?: "mutation" | "query";
 }
 
 export default class QueueLink extends ApolloLink {
-    private opQueue: OperationQueueEntry[] = [];
-    private isOpen = true;
+  private opQueue: OperationQueueEntry[] = [];
+  private isOpen = true;
 
-    public open() {
-        this.isOpen = true;
-        this.opQueue.forEach(({ operation, forward, observer }) => {
-            forward(operation).subscribe(observer);
+  private store: PersistentStore<string>;
+  private filter: string;
+  private storeKey: string;
+
+  public constructor(options?: QueueLinkOptions) {
+    super();
+    if (options) {
+      if (options.store) {
+        this.store = options.store.engine;
+        this.storeKey = options.store.storeKey;
+        this.restoreDataFromStore();
+      }
+      this.filter = options.filter;
+    }
+  }
+
+  public open() {
+    this.isOpen = true;
+    this.opQueue.forEach(({ operation, forward, observer }) => {
+      forward(operation).subscribe(observer);
+    });
+    this.opQueue = [];
+    if (this.store) {
+      this.store.removeItem(this.storeKey);
+    }
+  }
+
+  public close() {
+    this.isOpen = false;
+  }
+
+  public request(operation: Operation, forward: NextLink) {
+    if (this.isOpen) {
+      return forward(operation);
+    }
+    if (operation.getContext().skipQueue) {
+      return forward(operation);
+    }
+    return new Observable(observer => {
+      const operationEntry = { operation, forward, observer };
+      this.enqueue(operationEntry);
+      return () => this.cancelOperation(operationEntry);
+    });
+  }
+
+  private cancelOperation(entry: OperationQueueEntry) {
+    this.opQueue = this.opQueue.filter(e => e !== entry);
+    this.storeQueue();
+  }
+
+  private storeQueue() {
+    if (this.store) {
+      this.store.setItem(this.storeKey, JSON.stringify(this.opQueue));
+    }
+  }
+
+  private enqueue(entry: OperationQueueEntry) {
+    if (this.filter) {
+      if (this.matchesOperation(this.filter, entry.operation)) {
+        this.save(entry);
+      }
+    } else {
+      this.save(entry);
+    }
+  }
+
+  private save(entry: OperationQueueEntry) {
+    this.opQueue.push(entry);
+    this.storeQueue();
+  }
+
+  private matchesOperation(filter: string, operation: Operation): boolean {
+    return (
+      operation.query.definitions.filter(e => {
+        return (e as any).operation === filter;
+      }).length > 0
+    );
+  }
+
+  private restoreDataFromStore() {
+    if (this.store) {
+      const savedData = this.store.getItem(this.storeKey);
+      if (typeof savedData === "string") {
+        this.opQueue = JSON.parse(savedData as string);
+      }
+      if (typeof savedData === "object") {
+        savedData.then(data => {
+          this.opQueue = JSON.parse(data);
         });
-        this.opQueue = [];
+      }
     }
-
-    public close() {
-        this.isOpen = false;
-    }
-
-    public request(operation: Operation, forward: NextLink) {
-        if (this.isOpen) {
-            return forward(operation);
-        }
-        if (operation.getContext().skipQueue) {
-            return forward(operation);
-        }
-        return new Observable(observer => {
-            const operationEntry = { operation, forward, observer };
-            this.enqueue(operationEntry);
-            return () => this.cancelOperation(operationEntry);
-        });
-    }
-
-    private cancelOperation(entry: OperationQueueEntry) {
-        this.opQueue = this.opQueue.filter(e => e !== entry);
-    }
-
-    private enqueue(entry: OperationQueueEntry) {
-        this.opQueue.push(entry);
-    }
+  }
 }
